9d9e97043ab288cbbcd0385312c9dab2f8c4f45d
JonahPeck <PeckJonahC@gmail.com>
2023-02-03 13:34:55 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+document.addEventListener( "DOMContentLoaded", function () {
+    updateDOM()
+  } );
+  
+  
+  function updateDOM() {
+    document.getElementById( "text" )
+      .innerHTML = "This is really cool!";
+  }
+    // I know that I need to use textContent to replace the #text with the new variable of p
+
